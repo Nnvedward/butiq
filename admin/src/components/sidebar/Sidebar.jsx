011872613e75660bb2e837a1cc9468ad44b9f070
Nnvedward <nnvedward@gmail.com>
2022-03-20 20:44:15 +0100

    Added admin dashboard

---
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,95 @@
+import './sidebar.css'
+
+import React from 'react'
+import { Home, BarChart, ChatBubbleOutline, CurrencyExchange, DynamicFeed, MailOutline, PermIdentity, Report, Storefront, Timeline, TrendingUp, WorkOutline } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='sidebarWrapper'>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Dashboard</h3>
+                    <ul className='sidebarList'>
+                        <Link to='/' className='link'>
+                            <li className='sidebarListItem active'>
+                                <Home className='sidebarIcon' />
+                                home
+                            </li>
+                        </Link>
+                        <li className='sidebarListItem'>
+                            <Timeline className='sidebarIcon' />
+                            Analytics
+                        </li>
+                        <li className='sidebarListItem'>
+                            <TrendingUp className='sidebarIcon' />
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Quick menu</h3>
+                    <ul className='sidebarList'>
+                        <Link to='/users' className='link'>
+                            <li className='sidebarListItem'>
+                                <PermIdentity className='sidebarIcon' />
+                                Users
+                            </li>
+                        </Link>
+                        <Link to='/products' className='link'>
+                            <li className='sidebarListItem'>
+                                <Storefront className='sidebarIcon' />
+                                Products
+                            </li>
+                        </Link>
+                        <li className='sidebarListItem'>
+                            <CurrencyExchange className='sidebarIcon' />
+                            Transactions
+                        </li>
+                        <li className='sidebarListItem'>
+                            <BarChart className='sidebarIcon' />
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Notifications</h3>
+                    <ul className='sidebarList'>
+                        <li className='sidebarListItem'>
+                            <MailOutline className='sidebarIcon' />
+                            Mail
+                        </li>
+                        <li className='sidebarListItem'>
+                            <DynamicFeed className='sidebarIcon' />
+                            Feedback
+                        </li>
+                        <li className='sidebarListItem'>
+                            <ChatBubbleOutline className='sidebarIcon' />
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Staff</h3>
+                    <ul className='sidebarList'>
+                        <li className='sidebarListItem'>
+                            <WorkOutline className='sidebarIcon' />
+                            Manage
+                        </li>
+                        <li className='sidebarListItem'>
+                            <Timeline className='sidebarIcon' />
+                            Analytics
+                        </li>
+                        <li className='sidebarListItem'>
+                            <Report className='sidebarIcon' />
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ admin/src/data.js
@@ -0,0 +1,176 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000
+    },
+    {
+        name: 'Feb',
+        "Active User": 400
+    },
+    {
+        name: 'Mar',
+        "Active User": 3000
+    },
+    {
+        name: 'Apr',
+        "Active User": 2500
+    },
+    {
+        name: 'May',
+        "Active User": 4000
+    },
+    {
+        name: 'Jun',
+        "Active User": 2700
+    },
+    {
+        name: 'Jul',
+        "Active User": 3000
+    },
+    {
+        name: 'Aug',
+        "Active User": 600
+    },
+    {
+        name: 'Sept',
+        "Active User": 1200
+    },
+    {
+        name: 'Oct',
+        "Active User": 900
+    },
+    {
+        name: 'Nov',
+        "Active User": 1000
+    },
+    {
+        name: 'Dec',
+        "Active User": 4000
+    },
+];
+
+export const productData = [
+    {
+        name: 'Jan',
+        "Sales": 4000
+    },
+    {
+        name: 'Feb',
+        "Sales": 2000
+    },
+    {
+        name: 'Mar',
+        "Sales": 5000
+    }
+];
+
+export const userRows = [
+    {
+        id: 1,
+        username: 'Jon Snow',
+        avatar: 'https://i.ibb.co/fGsk7V7/IMG-2313.jpg',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$230.00'
+    },
+    {
+        id: 2,
+        username: 'Jon Snow',
+        avatar: 'https://i.ibb.co/fGsk7V7/IMG-2313.jpg',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$230.00'
+    },
+    {
+        id: 3,
+        username: 'Jon Snow',
+        avatar: 'https://i.ibb.co/fGsk7V7/IMG-2313.jpg',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$230.00'
+    },
+    {
+        id: 4,
+        username: 'Jon Snow',
+        avatar: 'https://i.ibb.co/fGsk7V7/IMG-2313.jpg',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$230.00'
+    },
+    {
+        id: 5,
+        username: 'Jon Snow',
+        avatar: 'https://i.ibb.co/fGsk7V7/IMG-2313.jpg',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$230.00'
+    }
+];
+
+export const productRows = [
+    {
+        id: 1,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    },
+    {
+        id: 2,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    },
+    {
+        id: 3,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    },
+    {
+        id: 4,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    },
+    {
+        id: 5,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    },
+    {
+        id: 6,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    },
+    {
+        id: 7,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    },
+    {
+        id: 8,
+        name: 'Nike AirMax',
+        img: "https://i.ibb.co/tXwGcM1/IMG-2297.jpg",
+        stock: 12,
+        status: 'active',
+        price: '$230.00'
+    }
+];
+
