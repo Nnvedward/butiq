a6e2cc79abad41ef0c72e271899f56f836c9b08a
Nnvedward <nnvedward@gmail.com>
2022-03-21 13:27:13 +0100

    Setup admin login and persist

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,7 +5,8 @@ import Home from "./pages/home/Home";
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
@@ -13,37 +14,46 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
 
 const App = () => {
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.role === ("Admin" || "SuperAdmin")
   return (
     <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/" >
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newProduct">
-            <NewProduct />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/login">
+        {admin ? <Redirect to="/"/> : <Login />}
+        </Route>
+        { admin && (
+          <>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Route exact path="/" >
+                <Home />
+              </Route>
+              <Route path="/users">
+                <UserList />
+              </Route>
+              <Route path="/user/:userId">
+                <User />
+              </Route>
+              <Route path="/newUser">
+                <NewUser />
+              </Route>
+              <Route path="/products">
+                <ProductList />
+              </Route>
+              <Route path="/product/:productId">
+                <Product />
+              </Route>
+              <Route path="/newProduct">
+                <NewProduct />
+              </Route>
+            </div>
+          </>)
+        }
+      </Switch>
     </Router>
   );
 }
--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const API = 'http://localhost:4000/api/'
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.token
+console.log(TOKEN)
 
 export const publicRequest = axios.create({
     baseURL: API
