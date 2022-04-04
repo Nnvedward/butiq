8667e438e7e076df443bfca00c531f95c5cd833d
Nnvedward <nnvedward@gmail.com>
2022-04-04 14:14:19 +0100

    Cleaning up

--- api/src/routes/role.route.js
+++ api/src/routes/role.route.js
@@ -3,9 +3,5 @@ const RoleCtrl = require('../controllers/role.controller')
 
 router.post('/role', RoleCtrl.createRole)
 router.get('/:roleId', RoleCtrl.getRole)
-router.post('/permission', RoleCtrl.createPermission)
-router.get('/manage-permission/:roleId', RoleCtrl.managePermission)
-router.post('/manage-role-permission/:roleId', RoleCtrl.manageRolePermission)
-router.get('/assigned-permission/:roleId', RoleCtrl.getAssignedRolePermissions)
 
 module.exports = router
--- api/src/services/role.service.js
+++ api/src/services/role.service.js
@@ -1,6 +1,4 @@
 const Role = require('../models/role.model')
-const Permission = require('../models/permission.model')
-const RolePermission = require('../models/role-permission.model')
 const CustomError = require('../utils/custom-error')
 
 class RoleService {
@@ -23,78 +21,5 @@ class RoleService {
             role: role.name
         }
     }
-
-    // Create Permissions
-    async createPermission(data) {
-        const permission = new Permission(data)
-        await permission.save()
-
-        return data = {
-            permission: permission.name
-        }
-    }
-
-    // Manage permissions
-    async managePermission(roleId) {
-        const rolepermission = await RolePermission.findOne({ role: roleId })
-        const permissions = await Permission.find()
-
-        if (!rolepermission) {
-            permissions.forEach(async (permission) => {
-                await RolePermission({
-                    role: roleId,
-                    permission: permission._id
-                }).save();
-            })
-        }
-        else {
-            permissions.forEach(async (permission) => {
-                const permissionExistence = await RolePermission.findOne({ role: roleId, permission: permission });
-
-                if (!permissionExistence) {
-                    await RolePermission({
-                        role: roleId,
-                        permission: permission._id
-                    }).save();
-                }
-            })
-        }
-        // Get all permisions
-        const result = await RolePermission.find({ role: roleId }).populate("role permission", "-isAdmin")
-        return result
-    }
-
-    // Manage role-permissions
-    async manageRolePermission(roleId, requestRolePermissions) {
-
-        // Loop through the role-permission request
-        requestRolePermissions.forEach( async(reqRolePer) => {
-            const rolePermission = await RolePermission.findOne({ _id: reqRolePer._id })
-            if (!rolePermission) throw new CustomError('Role permission not found!', 404)
-
-            const data = {
-                isAssigned: reqRolePer.isAssigned
-            }
-
-            await rolePermission.updateOne(
-                { $set: data },
-                { new: true }
-            );
-        })
-
-        // Return updated data
-        const response = await RolePermission.find({ role: roleId }).populate('role permission', "-isAdmin")
-        return response
-    }
-
-    // Get all assigned role-permissions 
-    async getAssignedRolePermissions(roleId) {
-        const rolePermissions = await RolePermission.find({ role: roleId, isAssigned: true }).populate('role permission', "-isAdmin")
-
-        if (!rolePermissions) throw new CustomError('No assigned Role-Permissions!', 404)
-
-        return rolePermissions
-    }
 }
-
 module.exports = new RoleService()
